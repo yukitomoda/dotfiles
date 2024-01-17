24ea83aa1fc8aad0983b5477ada1a444ab07d32e
tomoda <tomoda@rook-jp.com>
2024-01-17 10:04:35 +0900

    見た目調整

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -10,8 +10,11 @@ config.default_prog = { 'nu', '-l' }
 
 -- 見た目
 config.color_scheme = 'Wombat'
+config.colors = {
+  scrollbar_thumb = '#226688'
+}
 config.window_background_opacity = 0.9
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 18
-
+config.enable_scroll_bar = true
 return config
