8b330bcdd40da13dbfd8021b68dab7b2f983edad
yt <ssqmuaalrle@gmail.com>
2024-01-22 23:12:30 +0900

    dotfiles.nu を使用した形式に変更

---
+++ dotfiles/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.default_prog = { 'nu', '-l' }
+
+-- 見た目
+config.front_end = 'WebGpu'
+config.color_scheme = 'Wombat'
+config.colors = {
+  scrollbar_thumb = '#226688'
+}
+config.window_background_opacity = 0.9
+config.font = wezterm.font 'Cica'
+config.font_size = 20
+config.enable_scroll_bar = true
+return config
