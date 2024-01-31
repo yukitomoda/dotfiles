2ae8b80004d584db4bda5924c069850f251824bd
yt <ssqmuaalrle@gmail.com>
2024-02-01 00:59:31 +0900

    update: add toggleterm, lazygit

---
+++ dotfiles/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,28 @@
+return {
+    'akinsho/toggleterm.nvim',
+    config = function()
+        require('toggleterm').setup{
+            open_mapping = [[<c-\>]]
+        }
+
+        local Terminal = require('toggleterm.terminal').Terminal
+        local lazygit = Terminal:new{
+            cmd = 'lazygit',
+            hidden = true,
+            direction = 'float',
+            on_open = function(term)
+                vim.cmd[[startinsert!]]
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+            end
+        }
+        local function lazygit_toggle()
+            lazygit:toggle()
+        end
+
+        vim.keymap.set('n', '<Leader>G', lazygit_toggle, { silent = true })
+        vim.keymap.set('n', '<Leader>#', function()
+            local term = Terminal:new{}
+            term:open()
+        end, { silent = true })
+    end,
+}
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 - [neovim](https://github.com/neovim/neovim/blob/master/INSTALL.md)
 - [Lua Language Server](https://luals.github.io)
 - [ripgrep](https://github.com/BurntSushi/ripgrep#installation)
+- [lazygit](https://github.com/jesseduffield/lazygit/)
